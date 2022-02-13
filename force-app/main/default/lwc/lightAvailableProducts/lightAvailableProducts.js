e9e15328d2d2565114544b19f32bed0964dd53ab
kubaGit <kuba.trasz@gmail.com>
2022-02-13 10:49:27 +0100

    AddToOrder button activates/deactivates based on Order status

--- force-app/main/default/lwc/lightAvailableProducts/lightAvailableProducts.js
+++ force-app/main/default/lwc/lightAvailableProducts/lightAvailableProducts.js
@@ -1,7 +1,7 @@
 import { LightningElement, wire, api, track } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { getRecord } from 'lightning/uiRecordApi';
-import { reduceErrors } from 'c/ldsUtils';
+//import { reduceErrors } from 'c/ldsUtils';
 
 import ORDER_STATUSCODE_FIELD from '@salesforce/schema/Order.StatusCode';
 import ORDER_PRICEBOOKID_FIELD from '@salesforce/schema/Order.Pricebook2Id';
@@ -10,47 +10,46 @@ import getAvailableProducts from '@salesforce/apex/AvailableProductsController.g
 
 import { publish, subscribe, MessageContext } from 'lightning/messageService';
 import PRODUCT_ADDED_CHANNEL from '@salesforce/messageChannel/productAddedToOrder__c';
-import ORDER_ACTIVATED_CHANNEL from '@salesforce/messageChannel/orderActivated__c';
+//import ORDER_ACTIVATED_CHANNEL from '@salesforce/messageChannel/orderActivated__c';
 
 const FIELDS = ['Order.Pricebook2Id', 'Order.StatusCode'];
-
+const COLS = [
+    {
+        label: 'Name', 
+        fieldName: 'ProductName', 
+        type: 'text'
+    },
+    {
+        label: 'List Price', 
+        fieldName: 'UnitPrice', 
+        type: 'currency',
+        initialWidth: 125
+    },
+    {
+        label: 'Order',
+        type: 'button-icon',
+        initialWidth: 75,
+        typeAttributes: {
+            iconName: 'utility:add',
+            title: 'Add to Order',
+            variant: 'border-filled',
+            alternativeText: 'Add to Order',
+            name: 'addToOrder',
+            disabled: false
+        }
+    }
+];
 
 export default class LightAvailableProducts extends LightningElement {
 
-    @track columns = [
-        {
-            label: 'Name', 
-            fieldName: 'ProductName', 
-            type: 'text'
-        },
-        {
-            label: 'List Price', 
-            fieldName: 'UnitPrice', 
-            type: 'currency',
-            initialWidth: 125
-        },
-        {
-            label: 'Order',
-            type: 'button-icon',
-            initialWidth: 75,
-            typeAttributes: {
-                iconName: 'utility:add',
-                title: 'Add to Order',
-                variant: 'border-filled',
-                alternativeText: 'Add to Order',
-                name: 'addToOrder',
-                disabled: false
-            }
-        }
-    ];
-
+    columns = COLS;
     subscription = null;
     pbId;
-    @track wiredData;
-    @track wiredOrder;
-    @track availableProds;
+    wiredData; //@track
+    wiredOrder; //@track
+    availableProds; //@track
     error;
-    @track orderStatus;
+    orderStatus; //@track
     @api recordId;
     @track canAddToOrder = true;
 
@@ -64,31 +63,16 @@ export default class LightAvailableProducts extends LightningElement {
             console.log('[AvailableProducts][getRecord] Getting Order record = ' +JSON.stringify(result));
             this.pbId = result.data.fields.Pricebook2Id.value;
             this.orderStatus = result.data.fields.StatusCode.value;
-            console.log('[AvailableProducts][getRecord] pbId = ' +this.pbId);
-            console.log('[AvailableProducts][getRecord] orderStatus = ' +this.orderStatus);
-            console.log('[AvailableProducts][getRecord] Columns[2] BEFORE = ' +JSON.stringify(this.columns[2]));
-            let tmpCols = JSON.parse(JSON.stringify(this.columns));
-            console.log('[AvailableProducts][getRecord] tmpCols = ' +JSON.stringify(tmpCols));
-
-            if (this.orderStatus == 'Activated') {
-                tmpCols[2].typeAttributes.disabled = true;
-            }
-            else {
-                tmpCols[2].typeAttributes.disabled = false;
-            }
-            this.columns = tmpCols;
-            console.log('[AvailableProducts][getRecord] Columns[2] AFTER = ' +JSON.stringify(this.columns[2]));
-            //refreshApex(this.wiredOrder);
-            console.log('[AvailableProducts][getRecord] Order column disabled? = ' +this.columns[2].typeAttributes.disabled);
-            //this.refreshActionButtons();
-            //this.columns[2].typeAttributes.disabled = result.data.fields.StatusCode.value == 'Activated' ? true : false;
             this.error = undefined;
-            console.log('@wire, getRecord: pdId=' +this.pbId);
+            // Update action button ([+]) to add products to orders depending on status of the order.
+            let tmpCols = JSON.parse(JSON.stringify(this.columns));
+            tmpCols[2].typeAttributes.disabled = this.orderStatus == 'Activated' ? true : false;
+            this.columns = tmpCols;
         }
         else if (result.error) {
             this.error = result.error;
             this.pbId = undefined;
-            console.log('@wire, getRecord: error=' +error);
+            console.log('[AvailableProducts][getRecord] Error while getting Order record = ' +JSON.stringify(error));
         }
     }
 
@@ -135,19 +119,10 @@ export default class LightAvailableProducts extends LightningElement {
     
 
     // Use standard lifecycle hook to subscribe to message channel
+    /*
     connectedCallback() {
         this.subscribeToMessageChannel();
     }
-
-    /*
-    renderedCallback() {
-        console.log('RENDEREDCALLBACK - columns=' +JSON.stringify(this.columns));
-        console.log('RENDEREDCALLBACK - columns[2]=' +JSON.stringify(this.columns[2]));
-        console.log('RENDEREDCALLBACK - columns[2].disabled=' +JSON.stringify(this.columns[2].typeAttributes.disabled));
-        this.columns[2].typeAttributes.disabled = true;
-        //this.columns.forEach(col => { col.typeAttributes.disabled = true });
-    }
-    */
 
     subscribeToMessageChannel() {
         console.log('[AvailableProductsLWC][subscribeToMessageChannel] Subscribing to ORDER_ACTIVATED_CHANNEL message');
@@ -163,6 +138,7 @@ export default class LightAvailableProducts extends LightningElement {
         this.canAddToOrder = false;
         this.refreshActionButtons();
     }
+    */
 
 
     
